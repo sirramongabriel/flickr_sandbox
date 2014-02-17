6535c37108dddc4dd3b704e2d9e2061fd4e7c2ea
sirramongabriel <sirramongabriel@gmail.com>
2014-02-17 14:29:56 -0500

    Create flickr_helper

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# add Rails root
+require File.dirname(__FILE__) + '/../config/environment.rb'
+puts Rails.root
+
+# flickr-fu gem
+require 'flickr_fu'
+
 # Initialize the rails application
 FlickrSandbox::Application.initialize!
