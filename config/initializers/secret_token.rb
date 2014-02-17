626751c5bf966bcb53ced6ccccb4832fe308d39c
sirramongabriel <sirramongabriel@gmail.com>
2014-02-17 11:34:59 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlickrSandbox::Application.config.secret_token = 'ef3bf514f7516afa469febf2c1a91048ce1ba797b625a90dd7426bdf6719ffb78f7fcf299824f17548ad18302a8b4bfc560ebe3bd09a291acd0641728ff13626'
